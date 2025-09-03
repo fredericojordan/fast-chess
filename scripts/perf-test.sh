6af3503127e411cdbecc4c21618dc83eb238851e
Frederico Jordan <fredericojordan@gmail.com>
2025-09-02 22:41:25 -0300

    redirect collective output

--- scripts/perf-test.sh
+++ scripts/perf-test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-./bin/fast-chess -f "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"  # opening pos
-./bin/fast-chess -f "r1bqk1nr/pppp1ppp/2n5/2b1p3/1PB1P3/5N2/P1PP1PPP/RNBQK2R b KQkq - 0 4"  # evans gambit
-./bin/fast-chess -f "kbK5/pp6/1P6/8/8/8/8/R7 w - - 0 1"  # a1a6 Morphy mate in 2
-./bin/fast-chess -f "5rk1/pp4pp/4p3/2R3Q1/3n4/2q4r/P1P2PPP/5RK1 b - - 1 23"  # c3g3 Marshall's Legendary Move
-./bin/fast-chess -f "6k1/pb3pp1/4p3/1p2N1PQ/4q3/1N5R/1Pr4P/5K1R b - - 0 1"  # e4h1 Ivanchuk's missed mate in 1 against Anand
-./bin/fast-chess -f "2n3k1/q4pp1/1p2p2p/4N3/n7/4PBP1/5P1P/1Q4K1 w - - 0 33"  # b1b5 Kasparov miss vs Karpov, 24th game Seville (b1d1 instead)
+{
+  ./bin/fast-chess -f "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"  # opening pos
+  ./bin/fast-chess -f "r1bqk1nr/pppp1ppp/2n5/2b1p3/1PB1P3/5N2/P1PP1PPP/RNBQK2R b KQkq - 0 4"  # evans gambit
+  ./bin/fast-chess -f "kbK5/pp6/1P6/8/8/8/8/R7 w - - 0 1"  # a1a6 Morphy mate in 2
+  ./bin/fast-chess -f "5rk1/pp4pp/4p3/2R3Q1/3n4/2q4r/P1P2PPP/5RK1 b - - 1 23"  # c3g3 Marshall's Legendary Move
+  ./bin/fast-chess -f "6k1/pb3pp1/4p3/1p2N1PQ/4q3/1N5R/1Pr4P/5K1R b - - 0 1"  # e4h1 Ivanchuk's missed mate in 1 against Anand
+  ./bin/fast-chess -f "2n3k1/q4pp1/1p2p2p/4N3/n7/4PBP1/5P1P/1Q4K1 w - - 0 33"  # b1b5 Kasparov miss vs Karpov, 24th game Seville (b1d1 instead)
+} >/dev/null
