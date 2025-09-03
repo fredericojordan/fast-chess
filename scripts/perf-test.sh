e796ae34fda6cf8e1725579a40279a4df96acb82
Frederico Jordan <fredericojordan@gmail.com>
2025-09-02 21:31:05 -0300

    set cmake output dir

--- scripts/perf-test.sh
+++ scripts/perf-test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-./chess -f "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" >> /dev/null
-./chess -f "r1bqk1nr/pppp1ppp/2n5/2b1p3/1PB1P3/5N2/P1PP1PPP/RNBQK2R b KQkq - 0 4" >> /dev/null
+./bin/fast-chess -f "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" >> /dev/null
+./bin/fast-chess -f "r1bqk1nr/pppp1ppp/2n5/2b1p3/1PB1P3/5N2/P1PP1PPP/RNBQK2R b KQkq - 0 4" >> /dev/null
+./bin/fast-chess -f "kbK5/pp6/1P6/8/8/8/8/R7 w - - 0 1" >> /dev/null
